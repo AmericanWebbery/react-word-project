659d14e40e23ee372d761df25a6c025299772425
Jason <jasonetaylor@gmail.com>
2020-01-14 18:04:02 -0500

    added fcuntionality to fetch random word

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const WORD_API_KEY = "ZHO35KTY";
+  const WORD_API = `https://random-word-api.herokuapp.com/word?key=${WORD_API_KEY}&number=1`;
+
+  const [word, setWord] = useState("");
+
+  const FETCH_WORD = () => {
+    fetch(WORD_API)
+      .then(response => {
+        return response.json();
+      })
+      .then(response => setWord(response));
+  };
+
+  useEffect(() => {
+    FETCH_WORD();
+  }, []);
+
+  console.log(word);
+
   return (
     <div className="App">
-      <h1>react-word-project</h1>
+      <h1>REACT-WORD-PROJECT</h1>
+      <p>
+        A random word will be fetched from an online random word generator and a
+        second request will be made to the giphy API to return a gif based on
+        that word.
+      </p>
+      <p>
+        The word is: <span className="word">{word}</span>
+      </p>
     </div>
   );
 }
