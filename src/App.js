f2ec855bc8a2005f9edb552cea96ae67ccb2382a
Jason <jasonetaylor@gmail.com>
2020-01-14 17:22:19 -0500

    intial commit after CRA install -- probably left some bullshit in the public folder

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>react-word-project</h1>
+    </div>
+  );
+}
+
+export default App;
